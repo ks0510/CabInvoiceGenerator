fa4b58dcd10f869151306aff33a065482660fe28
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-05-24 18:05:19 +0530

    Added:The invoice generator should  now take in multiple ride and calculate  the agregate total for all

--- src/main/java/com/cabinvoice/CabInvoiceGenerator.java
+++ src/main/java/com/cabinvoice/CabInvoiceGenerator.java
@@ -1,17 +1,30 @@
 package com.cabinvoice;
 
 public class CabInvoiceGenerator {
-
 	public static final double costperKm = 10.0;
 	public static final double costpermin = 1.0;
 	public static final double min_fare = 5.0;
-	
+	private double totalFare; // Instance variable to store the total fare for all rides
+
 	public double calculateFare(double distance,int time) {
-		double totalFare = (costperKm*distance)+(costpermin*time);
+		double rideFare = (costperKm * distance) + (costpermin * time);
+		// Calculate fare for an individual ride
 		if(totalFare<5) {
 			return min_fare;
 		}
 		return totalFare;
 	}
 
+
+	public void calculateTotalFare(Ride[] rides) {
+		totalFare = 0;   // Initialize the total fare to zero
+
+		for (Ride ride : rides) {
+			totalFare += calculateFare(ride.distance, ride.time);
+			// Calculate fare for each ride and accumulate the total fare
+		}
+	}
+	public double getTotalFare() {
+		return totalFare;   // Return the total fare for all rides
+	}
 }
---
+++ src/main/java/com/cabinvoice/Ride.java
@@ -0,0 +1,13 @@
+package com.cabinvoice;
+
+public class Ride {
+
+    public double distance;
+    public int time;
+
+// Generate constructor
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
